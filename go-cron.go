c1fbb8d5de433ce4a74d04edc8077341ca47ce0e
Jeff Kolb <jeff.kolb@intlfctone.com>
2019-02-06 11:09:32 -0600

    Added start/stop logging

--- go-cron.go
+++ go-cron.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/robfig/cron"
 	//"gopkg.in/robfig/cron.v2"
@@ -35,7 +36,9 @@ func stop(c *cron.Cron, wg *sync.WaitGroup) {
 }
 
 func execute(command *cronEntry) {
-
+	t := time.Now().Format(time.RFC1123)
+	fmt.Printf("Running %v%v at %v\n", command.Application,
+		strings.Join(command.Args, " "), t)
 	// ToDo: Catch error starting
 	cmd := exec.Command(command.Application, command.Args...)
 	cmd.Stdout = os.Stdout
@@ -43,6 +46,10 @@ func execute(command *cronEntry) {
 	cmd.Run()
 	//ToDo: Catch error stopping
 	cmd.Wait()
+
+	t = time.Now().Format(time.RFC1123)
+	fmt.Printf("Done running %v%v at %v\n", command.Application,
+		strings.Join(command.Args, " "), t)
 }
 
 func getCronEntries() (entries []cronEntry) {
