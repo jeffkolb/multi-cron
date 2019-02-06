512b08382061e29a015974761b9799d8afdc98ac
Jeff Kolb <jeff.kolb@intlfctone.com>
2019-02-05 23:14:55 -0600

    I think it finally works now!

--- go-cron.go
+++ go-cron.go
@@ -9,44 +9,19 @@ import (
 	"sync"
 	"syscall"
 
-	//"github.com/robfig/cron"
-	"gopkg.in/robfig/cron.v2"
+	"github.com/robfig/cron"
+	//"gopkg.in/robfig/cron.v2"
 )
 
-func execute(command string, args []string) {
-
-	println("Doing Thangs")
-	println("executing:", command, strings.Join(args, " "))
-
-	cmd := exec.Command(command, args...)
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	cmd.Run()
-
-	cmd.Wait()
-}
-
-func create() (cr *cron.Cron, wgr *sync.WaitGroup) {
-	var schedule = os.Args[1]
-	var command = os.Args[2]
-	var args = os.Args[3:len(os.Args)]
-
-	execute(command, args)
-	wg := &sync.WaitGroup{}
-	c := cron.New()
-	println("new cron:", schedule)
-	c.AddFunc(schedule, func() {
-		wg.Add(1)
-		execute(command, args)
-		wg.Done()
-	})
-
-	return c, wg
+type cronEntry struct {
+	Schedule     string
+	Application  string
+	Args         []string
+	RunAtStartup bool
 }
 
 func start(c *cron.Cron, wg *sync.WaitGroup) {
+
 	c.Start()
 }
 
@@ -59,47 +34,94 @@ func stop(c *cron.Cron, wg *sync.WaitGroup) {
 	os.Exit(0)
 }
 
-func main() {
+func execute(command *cronEntry) {
+
 	/*
-			All key/value pairs for debugging purposes
-			for _, e := range os.Environ() {
-				pair := strings.Split(e, "=")
-				// pair[0] is key, pair[1] is value
-				fmt.Println(pair[0] + "=" + pair[1])
-		    }
+		t := time.Now().Format(time.RFC1123)
+		fmt.Printf("Executing %v%v at %v\n", command.Application,
+		strings.Join(command.Args, " "), t)
 	*/
+	// ToDo: Catch error starting
+	cmd := exec.Command(command.Application, command.Args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+	//ToDo: Catch error stopping
+	cmd.Wait()
+}
 
-	// ToDo: Test Connections to Azure and the target database
+func getCronEntries() (entries []cronEntry) {
+	cronEntries := make([]cronEntry, 0)
+	runAtStartup := 0
+	// Get all cron schedules defined in environmental variables.
+	for _, e := range os.Environ() {
+		var entry cronEntry
+		sch := strings.Split(e, "=")[1]
+		if strings.HasPrefix(e, "CRON_SCH") {
+			cmdKey := strings.Replace(strings.Split(e, "=")[0], "_SCH_", "_CMD_", 1)
+			argsKey := strings.Replace(strings.Split(e, "=")[0], "_SCH_", "_ARGS_", 1)
+			cmdVal, cmdExists := os.LookupEnv(cmdKey)
+			if cmdExists {
+				//Ones starting with '!' are run at startup
+				if strings.HasPrefix(sch, "!") {
+					entry.RunAtStartup = true
+					runAtStartup++
+				}
+				entry.Schedule = strings.TrimLeft(sch, "!")
+				entry.Application = cmdVal
+				argsVal, argsExist := os.LookupEnv(argsKey)
+				if argsExist {
+					entry.Args = strings.Split(argsVal, " ")
+				}
+				cronEntries = append(cronEntries, entry)
+			} else {
+				fmt.Printf("No command exists for %v\n", strings.Split(e, "=")[0])
+			}
+		}
+		_ = entry
+	}
 
-	keyVal, shouldRun := os.LookupEnv("CLEAN_AT_START")
-	if shouldRun && strings.ToUpper(keyVal) == "TRUE" {
-		// Run clean at start
-		fmt.Println("Running clean at application start.")
-	}
-	keyVal, shouldRun = os.LookupEnv("BACKUP_AT_START")
-	if shouldRun && strings.ToUpper(keyVal) == "TRUE" {
-		// Run backup at start
-		println("Running backup at application start.")
-	}
-	keyVal, shouldRun = os.LookupEnv("EMPTY_KEY")
-	if shouldRun && strings.ToUpper(keyVal) == "TRUE" {
-		println("Running backup at application start.")
-	}
-	keyVal, shouldRun = os.LookupEnv("KEY_NOT_PRESENT")
-	if shouldRun && strings.ToUpper(keyVal) == "TRUE" {
-		println("Running backup at application start.")
-	}
-	_ = keyVal
-	_ = shouldRun
+	fmt.Printf("%v cron schedules declared\n", len(cronEntries))
+	fmt.Printf("%v commands are configured to run at startup. \n", runAtStartup)
+	return cronEntries
+}
 
-	c, wg := create()
-	// - Schedule them tasks!
-	// - Write warnings and such on errors? Maybe kill the container?
-	go start(c, wg)
+func main() {
+	//wg := &sync.WaitGroup{}
+
+	//c.AddFunc("@every 10s", func() { fmt.Println("Every 10s") })
+	//c.AddFunc("@every 15s", func() { fmt.Println("Every 15s") })
+	// ToDo: Error Handling like WHOA
+	cronEntries := getCronEntries()
+	for _, e := range cronEntries {
+		var instance cronEntry
+		instance.Schedule = e.Schedule
+		instance.Application = e.Application
+		instance.Args = e.Args
+		instance.RunAtStartup = e.RunAtStartup
+
+		if instance.RunAtStartup {
+			fmt.Printf("running \"%v %v\" at startup\n", instance.Application,
+				strings.Join(instance.Args, " "))
+			execute(&instance)
+		}
+
+		c := cron.New()
+		c.AddFunc(e.Schedule, func() { execute(&instance) })
+		c.Start()
+		_ = c
+		_ = e
+	}
+
+	// ToDo: the cronEntries slice should return in the numerical order
+	// but we should sort the slice by number.
+	// No one should ever have loads of cron entries because that wouldn't be "Dockeresque".
+
+	// - ToDo: Write warnings and such on errors? Maybe kill the container?
 
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	println(<-ch)
-
-	stop(c, wg)
+	// ToDo: Write a graceful termination
+	// stop(c, wg)
 }
